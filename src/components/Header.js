d186a9b4df767e9815f0da245829694eea315d0e
Diego Avila <diegoavila358@gmail.com>
2024-01-30 10:56:58 -0500

    -Made code prettier, gonna merge to main because its too many changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import logoImage from '../assets/images/WhiteTD.png';
-import './header.css';
-import './styles.css';
+import '../assets/stylings/header.css';
+import '../assets/stylings/generalStyles.css';
 import 'bootstrap';
 
 function Header() {
@@ -17,36 +17,87 @@ function Header() {
   };
 
   return (
-    <div className='header'>
-      <nav className="header-navbar navbar" style={{ margin: 0, padding: 0 }}>
-        <div className="container ml-0">
-          <Link to="/" className="navbar-brand" onClick={() => handleLinkClick('/')}> 
-            <img src={logoImage} alt="Logo" width="auto" height="auto" className="d-inline-block align-top" />
+    <div
+      className='header'
+      style={{
+        minWidth: 'var(--min-width)',
+        position: 'relative',
+        margin: 0,
+        padding: 0,
+        boxSizing: 'border-box',
+        display: 'flex',
+        width: 'inherit',
+      }}>
+      <nav
+        className='header-navbar navbar'
+        style={{
+          margin: 0,
+          padding: 0,
+          borderBottom: '5px red solid',
+          backgroundColor: 'black',
+          width: '100%',
+          height: '75px',
+          justifyContent: 'center',
+          display: 'flex',
+          flexWrap: 'nowrap',
+          alignItems: 'center',
+        }}>
+        <div className='container-fluid' style={{ width: 'inherit' }}>
+          <Link
+            to='/'
+            className='navbar-brand'
+            onClick={() => handleLinkClick('/')}>
+            <img
+              src={logoImage}
+              alt='Logo'
+              width='auto'
+              height='auto'
+              className='d-inline-block align-top'
+            />
           </Link>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link to="/" className="nav-link" onClick={() => handleLinkClick('/')}>
+          <ul
+            className='navbar-nav'
+            style={{ flexDirection: 'row', color: 'white' }}>
+            <li className='nav-item'>
+              <Link
+                to='/'
+                className='nav-link'
+                onClick={() => handleLinkClick('/')}>
                 Home
               </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-link" onClick={() => handleLinkClick('/about')}>
+            <li className='nav-item'>
+              <Link
+                to='/about'
+                className='nav-link'
+                onClick={() => handleLinkClick('/about')}>
                 About
               </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/login" className="nav-link" onClick={() => handleLinkClick('/login')}>
+            <li className='nav-item'>
+              <Link
+                to='/login'
+                className='nav-link'
+                onClick={() => handleLinkClick('/login')}>
                 Tech Support
               </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/contact" className="nav-link" onClick={() => handleLinkClick('/contact')}>
+            <li className='nav-item'>
+              <Link
+                to='/contact'
+                className='nav-link'
+                onClick={() => handleLinkClick('/contact')}>
                 Contact Us
               </Link>
             </li>
           </ul>
-          <form className="d-flex ml-auto">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <form className='d-flex ml-auto'>
+            <input
+              className='form-control me-2'
+              type='search'
+              placeholder='Search'
+              aria-label='Search'
+            />
           </form>
         </div>
       </nav>
